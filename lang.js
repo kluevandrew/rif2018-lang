50b637d0433186ddcceccffd7247fd5226ef62b2
a.kluev <a.kluev@php73.ru>
2018-09-29 18:24:29 +0400

    optimizer

--- lang.js
+++ lang.js
@@ -8,20 +8,29 @@ const lexer = require('./lib/lexer');
 const parser = require('./lib/parser');
 const compiler = require('./lib/compiler');
 const interpreter = require('./lib/interpreter');
+const optimizer = require('./lib/optimizer');
 
 const code = fs.readFileSync(process.argv[2]).toString();
 
-
+console.log("============== tokens ================");
 const tokens = lexer.tokenize(code);
 console.log(util.inspect(tokens));
 console.log("==============================");
 
+
+console.log("=============== ast ===============");
 const ast = parser.parse(tokens);
 console.log(util.inspect(ast, { depth: null }));
 console.log("==============================");
 
+console.log("============== Opcodes ================");
 const opcodes = compiler.compile(ast);
 console.log(util.inspect(opcodes, { depth: null }));
 console.log("==============================");
 
-const result = interpreter.execute(opcodes);
+console.log("================ optimizedOpcodes ==============");
+const optimizedOpcodes = optimizer.optimize(opcodes);
+console.log(util.inspect(optimizedOpcodes, { depth: null }));
+console.log("==============================");
+
+const result = interpreter.execute(optimizedOpcodes);
--- test.py
+++ test.py
@@ -7,6 +7,16 @@ def main():
 
     print a + b
 
+class Cat:
+    def say(self, word):
+        print self.phrase(), word
+
+    def phrase(self):
+        return "Meow, "
+
+
+cat = Cat()
+cat.say("hello")
 
 c = Code.from_code(main.func_code)
 pprint(c.code)
